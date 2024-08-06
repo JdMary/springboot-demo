bc49a3c2117c54519e2c584c471bbb4e4142c574
jdMary <mariem.jrad@esprit.tn>
2024-08-06 13:36:35 +0100

    thymelead dep

--- src/main/java/com/example/demo/student/DBStudentService.java
+++ src/main/java/com/example/demo/student/DBStudentService.java
@@ -1,4 +1,40 @@
 package com.example.demo.student;
 
-public class DBStudentService implements S{
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class DBStudentService implements StudentService{
+    private final StudentRepository repository;
+
+    public DBStudentService(StudentRepository rep) {
+        this.repository = rep;
+    }
+
+
+    @Override
+    public List<Student> findAllStudents() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Student save(Student s) {
+        return repository.save(s);
+    }
+
+    @Override
+    public Student findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
+
+    @Override
+    public void delete(String email) {
+        repository.delete(findByEmail(email));
+    }
+
+    @Override
+    public Student update(Student s) {
+        return repository.save(s);
+    }
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,17 +1,18 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/students")
+@RequestMapping("/students")
 public class StudentController {
 
     private StudentService service;
 
-    public StudentController(StudentService service) {
+    public StudentController(@Qualifier("DBStudentService") StudentService service) {
         this.service = service;
     }
     @PostMapping
